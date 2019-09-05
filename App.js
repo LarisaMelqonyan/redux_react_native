c0ec8932467d7cc0b821046aafdaea49a41e0cc8
alina178 <larisa.melqonyan94@gmail.com>
2019-09-05 11:33:13 +0400

    Delete commented lines in App.js

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import TaskApp from './src/TaskApp'
-import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import store from './src/store'
 
@@ -23,50 +22,3 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
-///////////////////
-// import React, { Component } from 'react';
-// import PostForm from './crud/PostForm';
-// import AllPost from './crud/AllPost';
-// import {reducer} from './crud/reducers'
-//
-// const store = createStore(reducer)
-//
-// class App extends Component {
-//
-// render() {
-// return (
-//  <Provider store={store}>
-//     <PostForm />
-//     <AllPost />
-//   </Provider>
-// );
-// }
-// }
-// export default App;
-// import React from 'react';
-// import { StyleSheet, Text, View } from 'react-native';
-// import { createStore } from 'redux'
-// import { Provider } from 'react-redux'
-// import {reducer} from './crud/reducers'
-//
-// const store = createStore(reducer)
-//
-// export default class App extends React.Component {
-//   render() {
-//     return (
-//       <Provider store={store}>
-//          <PostForm />
-//          <AllPost />
-//        </Provider>
-//     );
-//   }
-// }
-//
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
